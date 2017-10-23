6111e32d59e8cfe9cbac9c0b40b6827dc792485c
rongyingjie@yovole.com <rongyingjie@yovole.com>
2017-10-23 22:51:31 +0800

    采用cdh 的jar

--- src/main/java/cn/gxufe/sparkStudy/scala/core/WordCountScala.scala
+++ src/main/java/cn/gxufe/sparkStudy/scala/core/WordCountScala.scala
@@ -14,7 +14,7 @@ object WordCountScala {
     val conf = new SparkConf().setMaster("local[2]").setAppName("WordCount");
     val sc = new SparkContext(conf);
 
-    val lines = sc.textFile("E:\\softPackage\\spark-1.6.1-bin-hadoop2.6\\README.md");
+    val lines = sc.textFile("D:\\softPackage\\spark-1.6.1-bin-hadoop2.6\\README.md");
 
     lines.flatMap( v => v.split(" ") ).map(word =>( word,1)).reduceByKey((v1,v2) => v1+v2).foreach(
      x => println(x._1 + "\t" + x._2 )
