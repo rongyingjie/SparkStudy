7375e956b0314956e80370055294fa40d07d32d8
rongyingjie <314062258@qq.com>
2017-11-12 20:55:05 +0800

    spark sql udf,udaf

--- src/main/java/cn/gxufe/spark/scala/sql/DailyUV.scala
+++ src/main/java/cn/gxufe/spark/scala/sql/DailyUV.scala
@@ -14,12 +14,10 @@ object DailyUV {
 
 
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("DailyUV");
-    val sc = new SparkContext(conf);
 
+    val conf = new SparkConf().setMaster("local[2]").setAppName("DailyUV")
+    val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
-
-
     val userAcc = Array(
       "2016-09-01,1",
       "2016-09-01,1",
@@ -28,23 +26,16 @@ object DailyUV {
       "2016-09-01,3",
       "2016-09-02,1",
       "2016-09-02,1")
-
     val data = sc.parallelize(userAcc).map(x => Row(x.split(",")(0),x.split(",")(1).toInt) )
     val structFields: util.List[StructField] = new util.ArrayList[StructField]()
 
     structFields.add(DataTypes.createStructField("name", DataTypes.StringType, true))
     structFields.add(DataTypes.createStructField("age", DataTypes.IntegerType, true))
     val structType: StructType = DataTypes.createStructType(structFields)
-
     val df = sqlContext.createDataFrame(data,structType)
-
     import sqlContext.implicits._
-
     df.registerTempTable("uv")
-
     df.groupBy("name").agg('name,countDistinct('age)).map(row => Row(row(1),row(2))).foreach( println )
-
-
     sc.stop()
 
   }
